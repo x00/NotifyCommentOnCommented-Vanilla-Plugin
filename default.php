4eae362127b75b4c71d7ade9b966e4bc5c337994
Paul Thomas <dt01pqt_pt@yahoo.com@yahoo.com>
2015-03-05 13:16:23 +0000

    fix email notifications

--- default.php
+++ default.php
@@ -3,8 +3,8 @@
 $PluginInfo['NotifyCommentOnCommented'] = array(
    'Name' => 'Notify Comment on Commented',
    'Description' => 'Notifies if someone has commented on a disicussion the user has commented on',
-   'Version' => '0.1.1b',
-   'RequiredApplications' => array('Vanilla' => '2.0.18'),
+   'Version' => '0.1.3b',
+   'RequiredApplications' => array('Vanilla' => '2.1'),
    'Author' => "Paul Thomas",
    'AuthorEmail' => 'dt01pq_pt@yahoo.com',
    'AuthorUrl' => 'http://vanillaforums.org/profile/x00',
@@ -12,30 +12,31 @@ $PluginInfo['NotifyCommentOnCommented'] = array(
 );
 
 class NotifyCommentOnCommented extends Gdn_Plugin {
-	public function ProfileController_AfterPreferencesDefined_Handler($Sender){
-		$Notifications = &$Sender->Preferences['Notifications'];
-		$Notifications['Email.CommentOnCommented'] =
-			T('Notify me when people reply to discussions I\'ve commented on.');
-		$Notifications['Popup.CommentOnCommented'] =
-			T('Notify me when people reply to discussions I\'ve commented on.');
-	}
-	
-	public function CommentModel_BeforeNotification_Handler($Sender, &$Args){
-		$Comment = &$Args['Comment'];
-		$Session = Gdn::Session();
-			
-		$ActivityModel = &$Args['ActivityModel'];
-		$Discussion = &$Args['Discussion'];
-		$DiscussionID = GetValue('DiscussionID', $Discussion);
-		$CommentID = GetValue('CommentID', $Comment);
-		
-		$NotifiedUsers = &$Args['NotifiedUsers'];
-		$UserModel = Gdn::UserModel();
-		
-		$Story = '['.$Discussion->Name."]\n".GetValue('Body', $Comment, '');
-		
-		
-		$UsersInDiscussion = $Sender->SQL
+    public function ProfileController_AfterPreferencesDefined_Handler($Sender){
+        $Notifications = &$Sender->Preferences['Notifications'];
+        $Notifications['Email.CommentOnCommented'] =
+            T('Notify me when people reply to discussions I\'ve commented on.');
+        $Notifications['Popup.CommentOnCommented'] =
+            T('Notify me when people reply to discussions I\'ve commented on.');
+    }
+    
+    public function CommentModel_BeforeNotification_Handler($Sender, &$Args){
+       
+        $Comment = &$Args['Comment'];
+        $Session = Gdn::Session();
+            
+        $ActivityModel = &$Args['ActivityModel'];
+        $Discussion = &$Args['Discussion'];
+        $DiscussionID = GetValue('DiscussionID', $Discussion);
+        $CommentID = GetValue('CommentID', $Comment);
+        
+        $NotifiedUsers = &$Args['NotifiedUsers'];
+        $UserModel = Gdn::UserModel();
+        
+        $Story = '['.$Discussion->Name."]\n".GetValue('Body', $Comment, '');
+        
+        
+        $UsersInDiscussion = $Sender->SQL
          ->Select('DISTINCT(c.InsertUserID) as InsertUserID')
          ->From('Comment c')
          ->Where('c.DiscussionID', $DiscussionID)
@@ -43,7 +44,7 @@ class NotifyCommentOnCommented extends Gdn_Plugin {
          
          
         foreach ($UsersInDiscussion->Result() as $Participant) {
-            if (in_array($Participant->InsertUserID, $NotifiedUsers) || $Participant->InsertUserID == $Session->UserID)
+            if (in_array($Participant->InsertUserID, $NotifiedUsers) || $Participant->InsertUserID == $Comment->InsertUserID)
                continue;
 
             $UserMayView = $UserModel->GetCategoryViewPermission($Participant->InsertUserID, $Discussion->CategoryID);
@@ -52,32 +53,31 @@ class NotifyCommentOnCommented extends Gdn_Plugin {
 
                $NotifiedUsers[] = $Participant->InsertUserID;
 
-               $ActivityID = $ActivityModel->Add(
-                  $Session->UserID,
-                  'CommentOnCommented',
-                  Anchor(Gdn_Format::Text($Discussion->Name), 'discussion/comment/'.$CommentID.'/#Comment_'.$CommentID),
-                  $Participant->InsertUserID,
-                  '',
-                  'discussion/comment/'.$CommentID.'/#Comment_'.$CommentID,
-                  FALSE
-               );
+                $ActivityID = $ActivityModel->Add(
+                    $Comment->InsertUserID,
+                    'CommentOnCommented',
+                    '',
+                    $Participant->InsertUserID,
+                    '',
+                    'discussion/comment/'.$CommentID.'/#Comment_'.$CommentID
+                );
                
                $ActivityModel->QueueNotification($ActivityID, $Story);
             }
          }
-	}
-	
-	public function Base_BeforeDispatch_Handler($Sender){
-		if(C('Plugins.'.$this->GetPluginIndex().'.Version')!=$this->PluginInfo['Version'])
-			$this->Setup();
-	}
-	
-	public function Setup(){
-		if (Gdn::SQL()->GetWhere('ActivityType', array('Name' => 'CommentOnCommented'))->NumRows() == 0)
-				Gdn::SQL()->Insert('ActivityType', array('AllowComments' => '0', 'Name' => 'CommentOnCommented', 'FullHeadline' => '%1$s commented on a %8$s you commented on.', 'ProfileHeadline' => '%1$s commented on a %8$s you commented on.', 'RouteCode' => 'discussion', 'Notify' => '1', 'Public' => '0'));
-		
-		SaveToConfig('Plugins.'.$this->GetPluginIndex().'.Version',$this->PluginInfo['Version']);
-	
-	}
-	
+    }
+    
+    public function Base_BeforeDispatch_Handler($Sender){
+        if(C('Plugins.'.$this->GetPluginIndex().'.Version')!=$this->PluginInfo['Version'])
+            $this->Setup();
+    }
+    
+    public function Setup(){
+        if (Gdn::SQL()->GetWhere('ActivityType', array('Name' => 'CommentOnCommented'))->NumRows() == 0)
+                Gdn::SQL()->Insert('ActivityType', array('AllowComments' => '0', 'Name' => 'CommentOnCommented', 'FullHeadline' => '%1$s commented on a %8$s you commented on.', 'ProfileHeadline' => '%1$s commented on a %8$s you commented on.', 'RouteCode' => 'discussion', 'Notify' => '1', 'Public' => '0'));
+        
+        SaveToConfig('Plugins.'.$this->GetPluginIndex().'.Version',$this->PluginInfo['Version']);
+    
+    }
+    
 }
